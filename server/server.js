f91237b7cfbb91f3fb32e0994e70aa493fc40ad9
unknown <chetansharma777555@gmail.com>
2025-07-21 16:44:49 +0530

    Update client

--- server/server.js
+++ server/server.js
@@ -6,12 +6,32 @@ import connectDB from './configs/mongodb.js';
 import imageRouter from './routes/imageRoutes.js';
 
 // App Config
+
 const PORT = process.env.PORT || 4000
 const app = express();
 await connectDB()
 
 // Intialize Middlewares
 app.use(express.json())
+
+const allowedOrigins = [
+  'https://clipify-frontend.vercel.app',
+];
+
+const corsOptions = {
+  origin: (origin, callback) => {
+    // The '|| !origin' part allows for tools like Postman to work
+    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  optionsSuccessStatus: 200
+};
+
+app.use(cors(corsOptions));
+
 app.use(cors())
 
 // API routes
